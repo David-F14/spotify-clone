888e6c86dac15f6807976d6db7b5bba17e1d0462
David F14 <davidfalue@gmail.com>
2023-11-20 17:27:38 +0100

    search view

---
+++ src/views/SearchView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import CategorySelect from '../components/CategorySelect.vue';
+
+</script>
+
+<template>
+    <div class="p-8">
+        <button
+            type="button"
+            class="text-white text-2xl font-semibold hover:underline cursor-pointer">
+            Recommanded Radio
+        </button>
+
+        <div class="py-1.5"></div>
+
+        <div class="flex items-center">
+            <CategorySelect category="Podcasts" image="https://picsum.photos/id/112/300/300" />
+        </div>
+    </div>
+</template>
+
