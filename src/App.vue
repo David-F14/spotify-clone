2f415871d7cb18d7a9e349b5c8371ca4c0bb1abd
David F14 <davidfalue@gmail.com>
2023-11-24 18:27:12 +0100

    add pinia

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
-  import { ref, onMounted } from 'vue'
+  import { ref } from 'vue'
   import { RouterLink, RouterView } from 'vue-router'
   import MenuItem from './components/MenuItem.vue';  
   import ChevronUp from 'vue-material-design-icons/ChevronUp.vue';
   import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
   import ChevronRight from 'vue-material-design-icons/ChevronRight.vue';
-  import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';
+  import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue';  
+
+  import { useSongStore } from './store/song';
+  import {storeToRefs} from 'pinia'
+
+
+  const useSong = useSongStore()
+  const { isPlaying, currentTrack } = storeToRefs(useSong)
 
   let openMenu = ref(false)
 </script>
@@ -105,6 +112,8 @@
         <div class="mt-[70px]"></div>
         <RouterView />
         <div class="mb-[100px]"></div>
+
+        
     </div>    
 </template>
 
--- src/store/song.js
+++ src/store/song.js
@@ -1,79 +1,79 @@
-import { defineStore } from 'pinia'
-import artist from '../artist.json'
+import { defineStore } from "pinia";
+import artist from "../artist.json";
 
-export const useSongStore = defineStore('song', {
+export const useSongStore = defineStore("song", {
     state: () => ({
         isPlaying: false,
         audio: null,
         currentArtist: null,
-        currentTrack: null
-      }),
+        currentTrack: null,
+    }),
 
-      actions: {
+    actions: {
         loadSong(artist, track) {
-            this.currentArtist = artist
-            this.currentTrack = track
-    
+            this.currentArtist = artist;
+            this.currentTrack = track;
+
             if (this.audio && this.audio.src) {
-                this.audio.pause()
-                this.isPlaying = false
-                this.audio.src = ''
+                this.audio.pause();
+                this.isPlaying = false;
+                this.audio.src = "";
             }
-    
-            this.audio = new Audio()
-            this.audio.src = track.path
-    
+
+            this.audio = new Audio();
+            this.audio.src = track.path;
+
             setTimeout(() => {
-                this.isPlaying = true
-                this.audio.play()
-            }, 200)
+                this.isPlaying = true;
+                this.audio.play();
+            }, 200);
         },
 
-        playOrPauseSong(){
+        playOrPauseSong() {
             if (this.audio.paused) {
                 this.isPlaying = true;
-                this.audio.play()
+                this.audio.play();
             } else {
                 this.isPlaying = false;
-                this.audio.pause()
+                this.audio.pause();
             }
         },
 
         playOrPauseThisSong(artist, track) {
-            if (!this.audio || !this.audio.src || (this.currentTrack.id !== track.id)){
+            if (!this.audio || !this.audio.src || this.currentTrack.id !== track.id) {
                 this.loadSong(artist, track);
-                return
+                return;
             }
 
-            this.playOrPauseSong()
+            this.playOrPauseSong();
         },
 
         prevSong(currentTrack) {
-            let track = artist.tracks[currentTrack.id - 2]
-            this.loadSong(artist, track)
+            let track = artist.tracks[currentTrack.id - 2];
+            this.loadSong(artist, track);
         },
-    
+
         nextSong(currentTrack) {
             if (currentTrack.id === artist.tracks.length) {
-                let track = artist.tracks[0]
-                this.loadSong(artist, track)
+                let track = artist.tracks[0];
+                this.loadSong(artist, track);
             } else {
-                let track = artist.tracks[currentTrack.id]
-                this.loadSong(artist, track)
+                let track = artist.tracks[currentTrack.id];
+                this.loadSong(artist, track);
             }
         },
 
-        playFromFirst(){
+        playFromFirst() {
             this.resetState();
             let track = artist.tracks[0];
             this.loadSong(artist, track);
         },
-        
-        resetState(){
+
+        resetState() {
             this.isPlaying = false;
             this.audio = null;
             this.currentArtist = null;
             this.currentTrack = null;
-        }
-    }
-})
+        },
+    },
+});
